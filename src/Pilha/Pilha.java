2bcb4cd06fe66edfa4912857b66b03bff744190b
caiotelesz <caioteles1267@gmail.com>
2023-08-30 17:19:41 -0300

    Sem o uso Stack

---
+++ src/Pilha/Pilha.java
@@ -0,0 +1,33 @@
+package Pilha;
+
+public class Pilha<T> extends EstruturaEstatica<T> {
+    public Pilha() {
+        super();
+    }
+
+    public Pilha(int capacidade) {
+        super(capacidade);
+    }
+
+    public void empilha(T elemento) {
+        super.adiciona(elemento);
+    }
+
+    public T topo() {
+        if(this.estaVazia()) {
+            return null;
+        } else {
+            return this.elementos[tamanho - 1];
+        }
+    }
+
+    public T desempilha() {
+        if(this.estaVazia()) {
+            return null;
+        }
+        T elemento = this.elementos[tamanho - 1];
+        tamanho--;
+        return elemento;
+    }
+}
+
